04766e1c5cdcefff23d454ca33825a844540aa1f
ibal <muammadibal@gmail.com>
2022-08-29 14:23:51 +0700

    update build gradle

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-native|@react-navigation|react-native-gesture-handler)',
-  ],
+  // transform: {
+  //   '^.+\\.(js|jsx)$': 'babel-jest',
+  //   '^.+\\.(ts|tsx)$': 'ts-jest',
+  // },
+  // transformIgnorePatterns: [
+  //   'node_modules/(?!(jest-)?react-native|@react-native-community|@react-native|@react-navigation|react-native-gesture-handler)',
+  // ],
 };
